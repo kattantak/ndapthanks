4beb9da573d1aebf0cacdd5f51a254ab8e11bea5
kattantak <57843727+kattantak@users.noreply.github.com>
2019-11-21 22:55:22 +0100

    4

--- app.py
+++ app.py
@@ -1,50 +1,31 @@
 # app.py
+import os
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
 
-    # For debugging
-    print(f"got name {name}")
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == os.environ['6mPhVZmqSZ57QFfMioqhl1Ra']
+    #is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
 
-    response = {}
+    return is_token_valid and is_team_id_valid
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
-    # Return the response in json format
-    return jsonify(response)
+@app.route('/', methods=['POST'])
+def hello_there():
+    if not is_request_valid(request):
+        abort(400)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+    return jsonify(
+        response_type='in_channel',
+        text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
+    )
+
 
 # A welcome message to test our server
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to NDAP Thanks!!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
