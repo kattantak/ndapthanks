37be99b5d8d9b18dd1d9c3ecea620b98ed48fea0
kattantak <57843727+kattantak@users.noreply.github.com>
2019-11-23 16:39:30 +0100

    print to logging and execution structure change

--- app.py
+++ app.py
@@ -29,50 +29,43 @@ def ndap_thanks():
         data = req_text.split(" ",2)
         try:
             thx_amount = int(data[0])
-            print(thx_amount)
+            logging.info(thx_amount)
+            if data[1][0] == '@':
+                thx_to_whom = data[1][1:]
+                logging.info(thx_to_whom)
+                thx_for_what = data[2]
+                logging.info(thx_for_what)
+                conn = None
+                try:
+                    #set windows env variable
+                    #set DATABASE_URL=postgres://vxkhlsqjazuwxi:9acac04cf61d6527f61d74c2036263653c7154934a04a7c24fa55d54f694d14c@ec2-54-228-207-163.eu-west-1.compute.amazonaws.com:5432/d6fhd2k9uscss7
+                    DATABASE_URL = os.environ['DATABASE_URL']
+                    # connect to the PostgreSQL server
+                    logging.info('Connecting to the PostgreSQL database...')
+                    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+                    # create a cursor
+                    cur = conn.cursor()
+                    # execute a statement
+                    postgres_insert_query = """INSERT INTO thanks_data (c_who, c_amount, c_to_whom, c_for_what) VALUES (%s, %s, %s, %s) RETURNING id;"""
+                    record_to_insert = (thx_who,thx_amount, thx_to_whom,thx_for_what)
+                    cur.execute(postgres_insert_query, record_to_insert)
+                    # display the PostgreSQL database SQL result
+                    db_sql_result = cur.fetchone()[0]
+                    logging.info('SQL Result: '+ str(db_sql_result))
+                    # commit the changes to the database
+                    conn.commit()
+                    # close the communication with the PostgreSQL
+                    cur.close()
+                except (Exception, psycopg2.DatabaseError) as error:
+                    logging.error(error)
+                finally:
+                    if conn is not None:
+                        conn.close()
+                        logging.info('Database connection closed.')
+            else:
+                logging.error(data[1] + " is not a valid '@mention' !")
         except ValueError:
-            print(data[0],"is not an int!")
-
-        if data[1][0] == '@':
-            thx_to_whom = data[1][1:]
-            print(thx_to_whom)
-        else:
-            print(data[1],"is not a valid '@mention' !")
-
-        thx_for_what = data[2]
-        print(thx_for_what)
-
-        conn = None
-        try:
-            #set windows env variable
-            #set DATABASE_URL=postgres://vxkhlsqjazuwxi:9acac04cf61d6527f61d74c2036263653c7154934a04a7c24fa55d54f694d14c@ec2-54-228-207-163.eu-west-1.compute.amazonaws.com:5432/d6fhd2k9uscss7
-            DATABASE_URL = os.environ['DATABASE_URL']
-            # connect to the PostgreSQL server
-            print('Connecting to the PostgreSQL database...')
-            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-            # create a cursor
-            cur = conn.cursor()
-
-            # execute a statement
-            postgres_insert_query = """INSERT INTO thanks_data (c_who, c_amount, c_to_whom, c_for_what) VALUES (%s, %s, %s, %s) RETURNING id;"""
-            record_to_insert = (thx_who,thx_amount, thx_to_whom,thx_for_what)
-            cur.execute(postgres_insert_query, record_to_insert)
-
-            # display the PostgreSQL database SQL result
-            db_sql_result = cur.fetchone()[0]
-            print('SQL Result:', db_sql_result)
-
-            # commit the changes to the database
-            conn.commit()
-
-            # close the communication with the PostgreSQL
-            cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-                print('Database connection closed.')
+            logging.error(data[0] + " is not an int!")
 
         return jsonify(
             response_type='in_channel',
